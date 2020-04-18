b147d182bb4aefa7a9b24a7891a86832cdddb69e
tokiya <tiki1091@outlook.jp>
2020-04-19 00:23:28 +0900

    ユーザーセルUI実装

--- GithubSearchUser/AppDelegate.swift
+++ GithubSearchUser/AppDelegate.swift
@@ -11,15 +11,17 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let searchUserViewController = R.storyboard.searchUser.instantiateInitialViewController()!
 
         let navigationController = UINavigationController(rootViewController: searchUserViewController)
 
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = navigationController
-        window.makeKeyAndVisible()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
 
         return true
     }
--- GithubSearchUser/SearchUser/View/SearchUserViewController.swift
+++ GithubSearchUser/SearchUser/View/SearchUserViewController.swift
@@ -16,17 +16,30 @@ final class SearchUserViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setup()
     }
 
-    /*
-     // MARK: - Navigation
+    func setup() {
+        tableView.register(R.nib.userTableViewCell)
+    }
+}
 
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+extension SearchUserViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.userCell.identifier) as! UserTableViewCell
+
+        return cell
+    }
+
+}
+
+extension SearchUserViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        print("test")
+    }
 }
