62c4db38f95b954ee0634aad399a1227302e0fc5
tokiya <tiki1091@outlook.jp>
2020-04-18 17:41:44 +0900

    検索用画面ファイル追加

---
+++ GithubSearchUser/SearchUser/Presenter/SearchUserPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  SearchUserPresenter.swift
+//  GithubSearchUser
+//
+//  Created by TOKIYA on 2020/04/18.
+//  Copyright © 2020 TOKIYA. All rights reserved.
+//
+
+import Foundation
+
+final class SearchUserPresenter {
+    
+}
