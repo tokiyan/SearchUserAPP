079accced2fc96ce772a2bfe19f49df8924aa662
tokiya <tiki1091@outlook.jp>
2020-04-18 17:06:17 +0900

    Initial Commit

---
+++ GithubSearchUser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubSearchUser
+//
+//  Created by TOKIYA on 2020/04/18.
+//  Copyright © 2020 TOKIYA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
