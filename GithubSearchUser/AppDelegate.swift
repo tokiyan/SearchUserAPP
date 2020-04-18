cbb9ee19e987ce19c1fa06a8fe4546de6b079901
tokiya <tiki1091@outlook.jp>
2020-04-18 19:08:10 +0900

    swiftlint 導入

--- GithubSearchUser/AppDelegate.swift
+++ GithubSearchUser/AppDelegate.swift
@@ -14,14 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let searchUserViewController = R.storyboard.searchUser.instantiateInitialViewController()!
-        
+
         let navigationController = UINavigationController(rootViewController: searchUserViewController)
-        
+
         let window = UIWindow(frame: UIScreen.main.bounds)
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
-        
+
         return true
     }
 }
-
--- GithubSearchUser/SearchUser/View/SearchUserViewController.swift
+++ GithubSearchUser/SearchUser/View/SearchUserViewController.swift
@@ -12,22 +12,21 @@ final class SearchUserViewController: UIViewController {
 
     @IBOutlet private weak var tableView: UITableView!
     @IBOutlet private weak var searchBar: UISearchBar!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
 
 }
