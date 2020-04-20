a3f68fd53b78c7d29f643b1a3a403ba5ae34d74f
tokiya <tiki1091@outlook.jp>
2020-04-21 02:33:31 +0900

    セルデザイン変更

---
+++ GithubSearchUser/Extension/AddBorderConfig.swift
@@ -0,0 +1,44 @@
+//
+//  AddBorderConfig.swift
+//  GithubSearchUser
+//
+//  Created by TOKIYA on 2020/04/21.
+//  Copyright © 2020 TOKIYA. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+
+    // 枠線の色
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return layer.borderColor.map { UIColor(cgColor: $0) }
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+
+    // 枠線のWidth
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    // 角丸設定
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+}
